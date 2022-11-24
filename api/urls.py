1b9ef7f3c3ef5e464343ae6af52da3b763d17e32
Abu <sufian@divine-it.net>
2022-11-24 18:45:07 +0600

    Pass Data from database using REST API

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.api_home),
+    path('courses/', views.course_list),
+    path('courses/<str:id>/', views.course_details),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.request import Request
+
+from dashboard.models import Course
+from dashboard.serializers import CourseSerializer
 
 # Create your views here.
 
@@ -11,3 +16,25 @@ def api_home(request, *args, **kargs):
     }
 
     return Response(res)
+
+@api_view(['GET'])
+def course_list(request:Request, *args, **kwargs):
+    res = {}
+    data = Course.objects.values()
+    print('data = ', data)
+    print(args)
+    print(kwargs)
+    return Response(data)
+
+@api_view(['GET'])
+def course_details(request:Request, *args, **kwargs):
+    print(request.query_params.values)
+    id = kwargs['id']
+    course:Course
+    try:
+        course = Course.objects.get(id = id)
+    except ObjectDoesNotExist:
+        return Response({})
+
+    serialize_course = CourseSerializer(course, many=False)
+    return Response(serialize_course.data)
